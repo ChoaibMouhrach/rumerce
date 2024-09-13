635e17330b1f092402d11c927ca716feb69babb3
ChoaibMouhrach <cmouhrach@gmail.com>
2024-09-13 01:03:03 +0100

    impr: remove sign_in test

--- tests/auth.rs
+++ tests/auth.rs
@@ -10,44 +10,6 @@ use tower::ServiceExt;
 mod common;
 
 #[tokio::test]
-async fn sign_in_works() {
-    let container = Postgres::default()
-        .with_tag("latest")
-        .start()
-        .await
-        .unwrap();
-
-    let (app, mut connection) = init(&container).await;
-
-    let email = "example@example.com";
-
-    let mut body = HashMap::new();
-    body.insert("email", email);
-
-    let response = app
-        .oneshot(
-            Request::builder()
-                .method("POST")
-                .uri("/sign-in")
-                .header("Content-Type", "application/json")
-                .body(Body::from(serde_json::to_string(&body).unwrap()))
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), 200);
-
-    let user = services::user::find_by_email(email, &mut connection)
-        .await
-        .unwrap()
-        .unwrap();
-
-    assert_eq!(user.user.email, email);
-    assert_eq!(user.role.name, ROLES.member);
-}
-
-#[tokio::test]
 async fn auth_works() {
     let container = Postgres::default()
         .with_tag("latest")
